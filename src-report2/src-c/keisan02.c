6316f10902376a6036c38bac9cd3e375acb62ab7
kzsh4124 <kazushi.02140413@gmail.com>
2022-06-08 00:41:41 +0900

    finish programmihg

--- src-report2/src-c/keisan02.c
+++ src-report2/src-c/keisan02.c
@@ -8,38 +8,26 @@ int getint(){
 
   return atoi(fgets(s, SSIZE, stdin));
 }
+int input_valid_int(char var_name[]){
+  int num;
+  while(1){
+    printf("input an interger number(%s): ",var_name);
+    num = getint();
+    if(2 <= num && num <= 9){
+      return num;
+    }else{
+      printf("Invalid value. Retype an integer in range 2-9.\n");
+    }
+  }
+}
 int main(void){
     int a,b,c;
-    // aの入力
-    while (1){
-      printf("input an interger number(a): ");
-      a = getint();
-      if(1<=a && a<=9){
-        break;
-      }else{
-        printf("Invalid value. Retype an integer in range 2-9.\n");
-      }
-    }
-    //bの入力
-    while(1){
-      printf("input an interger number(b): ");
-      b = getint();
-      if(1<=b && b<=9){
-        break;
-      }else{
-        printf("Invalid value. Retype an integer in range 2-9.\n");
-      }
-    }
-    //cの入力
-    while(1){
-      printf("input an interger number(c): ");
-      c = getint();
-      if(1<=c && c<=9){
-        break;
-      }else{
-        printf("Invalid value. Retype an integer in range 2-9.\n");
-      }
-    }
+    /* aの入力 */
+    a = input_valid_int("a");
+    /* bの入力 */
+    b = input_valid_int("b");
+    /* cの入力 */
+    c = input_valid_int("c");
     printf("value (a) is %d\n",a);
     printf("value (b) is %d\n",b);
     printf("value (c) is %d\n",c);
