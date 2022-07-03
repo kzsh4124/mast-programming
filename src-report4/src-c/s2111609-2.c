96901f3e2abb8089defb002275bb8c73931e36bc
kzsh4124 <kazushi.02140413@gmail.com>
2022-07-03 21:40:00 +0900

    complete question2

--- src-report4/src-c/s2111609-2.c
+++ src-report4/src-c/s2111609-2.c
@@ -54,6 +54,40 @@ void appendElement(struct LIST *l, int n){
     l->t = e;
 }
 
+/* insert node v after node p */
+void insert(struct LIST *l, struct Element *v, struct Element *p){
+    /* when p->next is NULL, NULL->prev causes Segmentation Error! */
+    if(p->next != NULL){
+        v->next = p->next;
+        p->next->prev = v;
+    }
+    p->next = v;
+    v->prev = p;
+    /* update tail element */
+    if(l->t == p){
+        l->t = v;
+    }
+}
+
+void insertElement(struct LIST *l, int n){
+    struct Element *e;
+    struct Element *seek;
+    seek = l->h;
+    e = getElement(n);
+    while(1){
+        /* when seek is the tail element */
+        if(seek->next == NULL){
+            insert(l, e, seek);
+            break;
+        /* when you detect the correct place to insert */
+        }else if(seek->next->val > n){
+            insert(l, e, seek);
+            break;
+        }else{
+            seek = seek->next;
+        }
+    }
+}
 
 void printAllElements(struct LIST *l){
     struct Element *e;
@@ -77,21 +111,24 @@ int getint(){
 }
 
 int main(int ac, char* av[]){
-    struct LIST *list;
+    struct LIST *list1, *list2;
     int i;
 
-    list = initList();
+    list1 = initList();
+    list2 = initList();
     while(1){
         printf("input a number (quit when 0): ");
         i = getint();
         if (i == 0){
             break;
         }
-        appendElement(list, i);
+        appendElement(list1, i);
+        insertElement(list2, i);
     }
 
-    printAllElements(list);
-    rprintAllElements(list);
+    printAllElements(list1);
+    puts("sorted");
+    printAllElements(list2);
 
 
 }
