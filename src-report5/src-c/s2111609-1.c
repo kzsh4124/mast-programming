cbbbdf469b7a3d26503c3c5e4f323028f304a3b9
kzsh4124 <kazushi.02140413@gmail.com>
2022-07-06 20:04:45 +0900

    debugging(not fixed)

--- src-report5/src-c/s2111609-1.c
+++ src-report5/src-c/s2111609-1.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #define BUFSIZE 100
 
+
+/* define linked list(datatype: char*) */
+
 struct Element {
     char* val;
     struct Element *prev;
@@ -13,7 +16,7 @@ struct LIST {
     struct Element *h;
     struct Element *t;
 };
-/* create new element (value: char* s)*/
+
 struct Element *getElement(char* s){
     struct Element *p;
 
@@ -44,7 +47,6 @@ struct LIST *initList(){
     return l;
 }
 
-/* add new element */
 void appendElement(struct LIST *l, char* s){
     struct Element *e;
 
@@ -58,9 +60,24 @@ void appendElement(struct LIST *l, char* s){
 
 void printAllElements(struct LIST *l){
     struct Element *e;
-
+    puts("{");
     for(e=l->h->next; e != NULL; e = e->next){
         printf("%s\n", e->val);
+    }
+    puts("}");
+}
+
+void free_list(struct LIST *l){
+    struct Element *now, *next;
+    now = l->h;
+    while(1){
+        free(now->val);
+        next = now->next;
+        free(now);
+        now = next;
+        if(now == NULL){
+            break;
+        }
     }
 }
 
@@ -73,28 +90,59 @@ char* chomp(char* s){
     }
 }
 
-int main(int ac, char* av[]){
-    struct LIST *list;
+
+/* convert csv formatted string into linked list */
+
+struct LIST* csv2list(char* str){
     int i;
-    char buf[BUFSIZE];
-    char* line;
+    char* token; 
+    char* head = str;
+    char delim = ',';
+    struct LIST* line = initList();
 
-    list = initList();
-    while(1){
-        if(fgets(buf, BUFSIZE, stdin) == NULL){
-            break;
-        }else{
-            chomp(buf);
+    chomp(str);
+    for(i=0; i<strlen(str)+1; i++){
+        if(str[i]== delim || str[i]=='\0'){
+            str[i] = '\0';
+            token = (char*) malloc(strlen(head)+1);
+            strcpy(token, head);
+            appendElement(line, token);
+            head = &str[i+1];
         }
-        line = (char*) malloc(strlen(buf)+1);
-        if(line == NULL){
-            puts("memory allocation failure!");
-            exit(EXIT_FAILURE);
-        }
-        strcpy(line, buf);
-        appendElement(list, line);
     }
-    puts("-----list content-----");
-    printAllElements(list);
+    return line;
+}
 
+
+int main(int ac, char* av[]){
+    FILE *fp;
+    char buf[BUFSIZE];
+    struct LIST* line_list;
+
+    if(ac != 2){
+        puts("invalid args!");
+        exit(EXIT_FAILURE);
+    }
+
+    fp = fopen(av[1], "r");
+    if(fp==NULL){
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        if(fgets(buf, BUFSIZE, fp) == NULL){
+            if(ferror(fp) != 0){
+                perror("fgets");
+                exit(EXIT_FAILURE);
+            }else{
+                break;
+            }
+        }
+
+        line_list = csv2list(buf);
+        printAllElements(line_list);
+        //free_list(line_list);
+    }
+    fclose(fp);
 }
